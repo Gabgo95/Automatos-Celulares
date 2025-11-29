2e45fcb79da3ea2c89553f6919212ce5cbca3ce2
Gabgo95 <gabrielb.faustino95@gmail.com>
2025-11-29 15:52:10 -0300

    Jogo da vida em AC

---
+++ AC.py
@@ -0,0 +1,43 @@
+"""#Jogo da vida"""
+
+# Tamanho da grade
+N = 20
+grid = np.random.choice([0, 1], size=(N, N))
+
+# Função para atualizar a grade
+def update(frameNum, img, grid, N):
+    newGrid = grid.copy()
+    for i in range(N):
+        for j in range(N):
+            # Soma dos vizinhos
+            total = int((
+                grid[i, (j-1)%N] + grid[i, (j+1)%N] +
+                grid[(i-1)%N, j] + grid[(i+1)%N, j] +
+                grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] +
+                grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N]
+            ))
+            # Regras
+            if grid[i, j] == 1:
+                if total < 2 or total > 3:
+                    newGrid[i, j] = 0
+            else:
+                if total == 3:
+                    newGrid[i, j] = 1
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+    return img,
+
+# Visualização
+fig, ax = plt.subplots()
+img = ax.imshow(grid, interpolation='nearest', cmap='gray')
+ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N), frames=200, interval=100)
+plt.show()
+
+# Criar figura
+fig, ax = plt.subplots()
+img = ax.imshow(grid, interpolation='nearest', cmap='gray')
+ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N), frames=200, interval=100)
+
+# Exibir no Colab
+HTML(ani.to_jshtml())
+
